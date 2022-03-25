1c512275f273e476586075ddf69b2a48d01635e1
Enzo7121 <enzo.lanus@gmail.com>
2022-03-25 13:50:07 -0300

    first phase hardcoded

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,39 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import {
+  ChakraProvider,
+  Container,
+  VStack,
+  Image,
+  Heading,
+  Text,
+  Box,
+  Divider,
+} from "@chakra-ui/react";
+import { AppProps } from "next/app";
+import theme from "../theme";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box padding={4}>
+        <Container
+          backgroundColor="white"
+          boxShadow="md"
+          borderRadius="sm"
+          maxWidth="container.xl"
+          padding={4}
+        >
+          <VStack marginBottom={6}>
+            <Image
+              src="https://media-exp1.licdn.com/dms/image/C4D0BAQHdpwBG9x1obw/company-logo_200_200/0/1619456100463?e=2147483647&v=beta&t=tpTVeFT-_ugxjOB3T4gU8oZNtqPXKv7c03Jyy2vZMM8"
+              alt="wea"
+            />
+          </VStack>
+          <Divider marginY={6} />
+          <Component {...pageProps} />
+        </Container>
+      </Box>
+    </ChakraProvider>
+  );
+};
 
-export default MyApp
+export default App;
---
+++ pages/product/api.tsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { Product } from "./types";
+import Papa from "papaparse";
+
+export default {
+  list: async (): Promise<Product[]> => {
+    return axios
+      .get(
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vQqMf8wgxLw4p27Z3HmNA4sRMBAITTB45ic8u9lZ_u-S_7k2eeXjjJLvMLVrrmx3u4g57wb5nbp8kMr/pub?output=csv",
+        {
+          responseType: "blob",
+        }
+      )
+      .then((response) => {
+        return new Promise<Product[]>((resolve, reject) => {
+          Papa.parse(response.data, {
+            header: true,
+            complete: (results) => {
+              const products = results.data as Product[];
+
+              return resolve(
+                products.map((product) => ({
+                  ...product,
+                  price: Number(product.price),
+                }))
+              );
+            },
+            error: (error) => {
+              return reject(error.message);
+            },
+          });
+        });
+      });
+  },
+};
